d59571a99b31226ef3851c5871494b170eb44a36
pahoffart25 <pahoffart25@gmail.com>
2020-02-10 15:16:31 -0600

    mandatory

--- db/migrate/20200210153347_recipes.rb
+++ db/migrate/20200210153347_recipes.rb
@@ -2,7 +2,7 @@ class Recipes < ActiveRecord::Migration[5.2]
   def change
     create_table :recipes do |t|
       t.string :title
-      t.string :description
+      t.text :description
       t.integer :calories
       t.integer :time
     end
